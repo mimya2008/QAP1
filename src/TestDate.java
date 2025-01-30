3ed144a993f0f5495e0f4a698567fd279c73ac2b
Mimya Hafiz <shantahafiz.mimya@gmail.com>
2025-01-30 01:31:31 -0330

    The First problem named as Account was done previously. The second problem is added now that about Date format.

--- src/TestDate.java
+++ src/TestDate.java
@@ -1,7 +1,7 @@
 public class TestDate {
     public static void main(String[] args) {
     Date D1 = new Date(5,3,1990);
-    Date D2 = new Date(13,13,2024);
+    Date D2 = new Date(1,13,2024);
     System.out.println(D1);
     System.out.println(D2);
     }
